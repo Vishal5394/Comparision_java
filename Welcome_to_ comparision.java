1e901a6e880e9ca3bd825fac53ce8aa569e6df0a
vishal5394 <vishaljadyal@gmail.com>
2022-07-03 12:57:41 +0530

    Welcom_to_comparision

---
+++ Welcome_to_ comparision.java
@@ -0,0 +1,10 @@
+package comarision;
+
+public class Welcom_to_comparision {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to line Comparison Computation Program");
+
+	}
+
+}
